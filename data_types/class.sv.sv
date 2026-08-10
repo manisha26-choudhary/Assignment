d1a04b6f5621bd2d5a743c2096e5bd9512d6800d
manisha <manisha26nagil@gmail.com>
2026-08-10 17:00:35 +0530

    Update class.sv.sv

--- data_types/class.sv.sv
+++ data_types/class.sv.sv
@@ -1,117 +1,102 @@
 // Code your design here
-// struct and union------------------------------------------------
-/*module tb();
-typedef struct packed{
-  int a;
-  bit[3:0] b;
-  logic[3:0]c;
-  //real d;
-} p_s;
-p_s ps;
-initial begin
-  ps=40'h1122334455;
- // ps.a=32'hAABBCCDD;
-  //ps.b=4'b0010;
-  //ps.c=4'b0101;
-  //ps.d=12.34;
-  $display("a=%0h",ps.a);
-  $display("b=%0h",ps.b);
-  $display("c=%0h",ps.c);
-  $display("entire struct=%0h",ps);
-  $display("size=%0d",$bits(ps));
-end
-endmodule*/
-/////////////////union////////////////////////////////////////////////
-/*module tb_1();
-typedef union packed{
-  int a;
-  //bit[3:0] b;
-  //logic[3:0]c;
-  struct packed{
-    logic[15:0] e;
-    bit [15:0] f;
-  } part;
-  //real d;
-} p_u;
-p_u pu;
-initial begin
-  pu.a=32'hAABBCCDD;
- // pu.b=4'b0010;
- // pu.c=4'b0101;
- //pu.d=12.34;
+/*class cons;
+   rand byte a[];
+  rand int s;
+ // int s=0;
+  constraint c1{a.size==5;}
+  //constraint c2{a.sum==500;}
+  constraint c2{foreach (a[i])
+    a[i] inside {[0:100]};
+               // a.sum==500;
+               // s+=a[i];
+               // s==500;
+                s==a[0]+a[1]+a[2]+a[3]+a[4];
+               s==500;
+               }
+endclass
 
-  $display("%0h",pu.a);
-  $display("%h",pu.part.e);
-  $display("entire union=%0h",pu);
-  $display("size=%0d",$bits(pu));
-end
-endmodule*/
-/////////////////////////struct_inside_class/////////////////////////////////
-module tb();
-  typedef struct packed {
-      int a;
-      bit[31:0]b;
-      logic[31:0] c;
-  } my_struct; 
-  
-  class transaction;
-     rand my_struct s1;
-    constraint c1{s1.b inside {[0:32]};}
-    function void display();
-      $display("a is %0h  b is %0h and c is %0h",s1.a,s1.b,s1.c);
-      $display("entire struct=%0p",s1);
-      $display("size=%0d",$bits(s1));      
-    endfunction
-  endclass
-  transaction tc;
+module tb;
+  cons c=new();
   initial begin
-    tc=new();
-    repeat(100) begin
-    tc.randomize();
-    tc.display();
+    if(c.randomize()) begin 
+  $display("array=%0p",c.a);
     end
+    else
+      $display("randomization failed");
   end
 endmodule
-////////////struct_union_hierarchy////////////////////////////////////////
-/*module tb();
-typedef union packed {
-  bit[31:0] raw;
-  struct packed {
-    bit[15:0] mani;
-    bit[15:0] rani;
-  }my_struct;
- // my_struct ms;
-}my_union;
-  typedef struct packed {
-  int a;
-  my_union mu;
-}half; 
-typedef struct packed {
-  int b;
-  half h;
-} my;
-  my m;
+//*****************************ASSOCIATIVE ARRAY METHODS*************************************************************
+module tb;
+int imem[int];
+  int idx;
   initial begin
-  m=96'h112233445566778899aabbccdd;
-    $display("a=%0h",m.h.a);
-    $display("b=%0h",m.b);
-    $display("raw=%0h",m.h.mu.raw);
-    $display("mani=%0h",m.h.mu.my_struct.mani);
-   // $display("entire struct=%0h",mu);
-   // $display("size=%0d",$bits(mu));
-end
+    
+imem[ 3 ] = 1;
+imem[ 16'hffff ] = 2;
+imem[ 4'b1000 ] = 3;
+//$display( "%0d entries\n", imem.num );
+    if(imem.exists(3))
+      imem.delete(3);
+   // $display("aa is %0p and %0d entries",imem, imem.num);
+   // $display("first value is %0h and last value is &0h",imem.first(idx),imem.last(idx));
+  end
 endmodule
-///////////////////////////assigning and retriving via diff.var///////////////////////
-module tb();
-  typedef union {
-    int a;
-    int b;
-  } my_union;
-  my_union mu1;
+//********************************QUEUE******************************************************************************
+module tb;
+  int mem[$]={10,20,30,40,50};
+  int temp[$];
   initial begin
-    mu1.a=32'habcd;
-    //mu2.b=mu1.a;
-    $display("value of a is %0d and b is %0d",mu1.a,mu1.b);
+   // temp=mem[1:3];
+   // temp=mem[2:$];
+   // temp={1,mem[2:$],2};
+    
+    $display("value is %0p",temp);
+  end
+endmodule
+//////////////////////manipulator method////////////////
+module tb;
+  int mm[string];
+  int aa[int];
+  int que[$]={1,2,3,4,5,6,7};
+  int i[$];
+  initial begin
+    aa[1]=1;
+    aa[6]=7;
+    aa[3]=3;
+   // i=aa.find(x) with (x>5);
+   //i=aa.find_index with (item==7);
+    i=aa.unique;
+    
+    $display("value is %0p",i);
+  end
+endmodule
+//************************ordering_method*************************
+module tb;
+  int aa[int];
+  int que[$];
+  int i[$];
+        initial begin
+      	aa[1]=1;
+    aa[6]=7;
+    aa[3]=3;
+         // aa.reverse;
+         // aa.sort;
+          aa.sort with (item ==7);
+          $display("value is %0p",aa);
+        end
+      
+endmodule
+//*********************reduction_method**************************
+module tb;
+  int aa[]={1,2,3,4,5};
+  logic[7:0] a[2][2]='{'{8'd1,8'd2},'{8'd2,8'd3}};
+  int que[$];
+  int i;
+  initial begin
+  	//i=aa.sum;
+   // i=aa.product;
+   // i=aa.xor with (item>3);
+    i= a.sum with (item.sum with (item)); 
+    $display("value is %0d",i);
   end
 endmodule*/
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
